bc07497f7f8449ddd4a8b1968b6c3932fa77f8c3
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-12-03 21:16:52 +0200

    Implemented model for User

---
+++ backend/src/models/user.js
@@ -0,0 +1,42 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password_hash: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    timestamps: false, // Disable Sequelize's automatic timestamps
+  }
+);
+
+module.exports = User;
