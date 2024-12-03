a668a3c5e8ca17034d520870baaca9db8d345761
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-12-03 20:39:03 +0200

    Init backend

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+const { PORT } = process.env; // Use an environment variable for the port
+
+// Set the default port if not defined in the environment
+const port = PORT || 5000;
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
