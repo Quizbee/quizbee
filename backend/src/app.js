596b4cbb6cf76950e820647b621a568a0f1d74b1
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2024-12-04 16:11:00 +0200

    Moved routes to separate files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
-const User = require('./models/user');
-const Deck = require('./models/deck');
-const Flashcard = require('./models/flashcard');
-const DeckFlashcard = require('./models/deckFlashcard');
+
+// Import routes
+const userRoutes = require('./routes/users');
+const deckRoutes = require('./routes/decks');
+const flashcardRoutes = require('./routes/flashcards');
+const deckFlashcardRoutes = require('./routes/deck_flashcards');
 
 require('dotenv').config();
 
@@ -15,52 +17,17 @@ app.use(express.json());
 
 // Routes
 app.get('/', (_, res) => {
-  res.send('Hello world!');
+  res.json({
+    message: 'Welcome to the Flashcards API!',
+    usage:
+      'Use the /api/users, /api/decks, and /api/flashcards endpoints to interact with the API.',
+  });
 });
 
-app.get('/api', (_, res) => {
-  res.json({ message: 'Welcome to the Flashcards API' });
-});
-
-app.get('/api/users', async (_, res) => {
-  try {
-    const users = await User.findAll(); // Await the result of the query
-    res.json({ users }); // Send the array of users as a JSON response
-  } catch (err) {
-    console.error('Error fetching users:', err);
-    res.status(500).json({ error: 'Unable to fetch users' });
-  }
-});
-
-app.get('/api/decks', async (_, res) => {
-  try {
-    const decks = await Deck.findAll(); // Await the result of the query
-    res.json({ decks }); // Send the array of decks as a JSON response
-  } catch (err) {
-    console.error('Error fetching decks:', err);
-    res.status(500).json({ error: 'Unable to fetch decks' });
-  }
-});
-
-app.get('/api/flashcards', async (_, res) => {
-  try {
-    const flashcards = await Flashcard.findAll(); // Await the result of the query
-    res.json({ flashcards }); // Send the array of flashcards as a JSON response
-  } catch (err) {
-    console.error('Error fetching flashcards:', err);
-    res.status(500).json({ error: 'Unable to fetch flashcards' });
-  }
-});
-
-app.get('/api/deck_flashcards', async (_, res) => {
-  try {
-    const deck_flashcards = await DeckFlashcard.findAll(); // Await the result of the query
-    res.json({ deck_flashcards }); // Send the array of deck_flashcards as a JSON response
-  } catch (err) {
-    console.error('Error fetching deck_flashcards:', err);
-    res.status(500).json({ error: 'Unable to fetch deck_flashcards' });
-  }
-});
+app.use('/api/users', userRoutes);
+app.use('/api/decks', deckRoutes);
+app.use('/api/flashcards', flashcardRoutes);
+app.use('/api/deckFlashcards', deckFlashcardRoutes);
 
 sequelize
   .sync() // Sync models
