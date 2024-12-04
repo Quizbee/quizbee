be88e083a913ff3021d384633654f786be349d87
hennasavilahti <savilahtihenna@gmail.com>
2024-12-04 18:49:43 +0200

    Added button to go back

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 const AuthPage = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className="flex min-h-screen flex-1 flex-col bg-black px-6 py-12 text-white lg:px-8">
-        <div className="mt-16 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="flex min-h-screen flex-1 flex-col bg-black px-6 py-8 text-white lg:px-8">
+        <button
+          className="justify-start-start mt-0 flex w-full"
+          onClick={() => navigate('/')}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+            />
+          </svg>
+          <a className="pl-4 text-xl">Back</a>
+        </button>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="text-center text-2xl/9 font-bold tracking-tight text-yellow-200">
             Sign in to your account
           </h2>
@@ -61,7 +84,7 @@ const AuthPage = () => {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-yellow-200 px-3 py-1.5 text-sm/6 font-semibold text-black shadow-sm hover:bg-yellow-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-300"
+                className="flex w-full justify-center rounded-md bg-yellow-200 px-3 py-1.5 text-sm/6 font-semibold text-black shadow-sm transition-all hover:scale-105 hover:bg-yellow-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-300"
               >
                 Sign in
               </button>
