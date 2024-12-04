b6152134b2e409fb7e374194cc38c45dfc865a87
hennasavilahti <savilahtihenna@gmail.com>
2024-12-04 14:31:16 +0200

    Finished Early Rootlayout for Router

---
+++ frontend/src/pages/RootLayout.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Outlet } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+
+const RootLayout = () => {
+  return (
+    <>
+      <MainNavigation />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <Outlet />
+    </>
+  );
+};
+
+export default RootLayout;
