de1e5b22e02fb905270242f268f82988a4fe7f1d
hennasavilahti <savilahtihenna@gmail.com>
2024-12-04 14:29:18 +0200

    Initialized React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import FrontPage from './pages/FrontPage/FrontPage';
+import RootLayout from './pages/RootLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+  },
+]);
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
